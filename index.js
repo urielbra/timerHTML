7cf02cccf2a843433b4d981cfa6ceef85c7cb4e0
Uriel B Braga <uriel.braga@enacom.com.br>
2019-09-09 22:19:41 -0300

    Agora em alpha

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 var iterance;
 var seconds;
+var timerRunning = false;
 var secondScreen = false;
+var swalOn = false;
+var audioPlaying;
 var firstButtonDisabled = true;
+
+var alarm = new Audio('alarm1.mp3');
+var duration = Math.ceil(alarm) * 1000;
+
 
 
 function getIterance(){
-    iterance = document.getElementById("iterance").value;
-    // seconds = document.getElementById("seconds").value;
+    iterance = get("iterance").value;
+    // seconds = get("seconds").value;
     if (iterance&&seconds){
         firstButtonDisabled = false;
-        document.getElementById("startButton").style.display = 'block';
-        document.getElementById("startButtonOff").style.display = 'none'; 
+        get("startButton").style.display = 'block';
+        get("startButtonOff").style.display = 'none'; 
    
     } else {
         firstButtonDisabled = true;
-        document.getElementById("startButton").style.display = 'none';
-        document.getElementById("startButtonOff").style.display = 'block'; 
+        get("startButton").style.display = 'none';
+        get("startButtonOff").style.display = 'block'; 
     
     }
 
 }
 
 function getSeconds(){
-    // iterance = document.getElementById("iterance").value;
-    seconds = document.getElementById("seconds").value;
+    // iterance = get("iterance").value;
+    seconds = get("seconds").value;
     if (iterance&&seconds){
         firstButtonDisabled = false;
-        document.getElementById("startButton").style.display = 'block';
-        document.getElementById("startButtonOff").style.display = 'none'; 
+        get("startButton").style.display = 'block';
+        get("startButtonOff").style.display = 'none'; 
     } else {
         firstButtonDisabled = true;
-        document.getElementById("startButton").style.display = 'none';
-        document.getElementById("startButtonOff").style.display = 'block'; 
+        get("startButton").style.display = 'none';
+        get("startButtonOff").style.display = 'block'; 
     
     }
 
@@ -40,9 +47,10 @@ function getSeconds(){
 
 function start() {
     secondScreen = true;
-    document.getElementById("firstScreen").style.display = 'none';
-    document.getElementById("secondScreen").style.display = 'block';
+    get("firstScreen").style.display = 'none';
+    get("secondScreen").style.display = 'block';
     setTime(toMinutes(seconds));
+    setIterance(iterance);
     console.log('Time to change screen')
 }
 
@@ -64,25 +72,78 @@ function toSeconds(minute){
 
 
 function setTime(actual){
-    document.getElementById("timer").innerText  = actual;
+    get("timer").innerText  = actual;
+}
+
+function setIterance(actual){
+    get("iteranceLeft").innerText  = actual;
 }
 
 function getTime(){
-    return document.getElementById("timer").innerText;
+    return get("timer").innerText;
 }
 
 function startTimer(){
     //  while(getTime() !== '00:00'){
      var running = setInterval(() => {
-        if (getTime() === '00:00') {
-            console.log('Time to stop');
-            clearInterval(running);
-        }
+         timerRunning = true;
         var oldSecond = getTime();
         var newSecond = (toSeconds(oldSecond)) - 1;
-        setTime(toMinutes(newSecond));    
+        if (getTime() === '00:00') {
+            clearInterval(running);
+            console.log('Time to stop');
+            iterance--;
+            spawnSWAL();
+            if(iterance===0){
+                location.reload();
+                timerRunning = false;
+                secondScreen = false;
+                firstButtonDisabled = true;
+                get("firstScreen").style.display = 'block';
+                get("secondScreen").style.display = 'none';
+                get("seconds").value = '';
+                get("iterance").value = '';
+            } else {
+            setIterance(iterance);
+            setTime(toMinutes(seconds));
+            timerRunning = false;
+            }
+        } else {    
+        setTime(toMinutes(newSecond));
+        }
     },1000);
     
     
     // }
+}
+
+function get(element){
+    return document.getElementById(element);
+}
+
+function spawnSWAL(){
+    swalOn = true;
+    get('swal').style.display = 'flex';
+    audioPlaying = setInterval(() => {
+        playAudio()
+    }, duration);
+
+}
+
+function closeSwal(){
+    clearInterval(audioPlaying);
+    pauseAudio();
+    (get('swal').style.display='none')
+    swalOn = false;
+}
+
+
+
+
+function playAudio() {
+  alarm.play();
+}
+
+function pauseAudio() {
+  alarm.pause();
 }
